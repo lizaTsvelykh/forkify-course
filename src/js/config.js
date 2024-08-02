2e1153607e2c961a931457e08e929c9c48c3d5b4
Liza Tsvelykh <lizatsvelykh@gmail.com>
2024-08-02 19:49:19 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "78aac973-54cd-4d0b-b440-2b0a0ace808d";
+export const MODAL_CLOSE_SEC = 2.5;
